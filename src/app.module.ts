b0743a2eb457b25554106bd8975cf3364ea0c40c
marlleyck <marlleyck@hotmail.com>
2023-01-28 17:11:40 -0300

    feat: create pokemon resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [PokemonModule],
   controllers: [AppController],
   providers: [AppService],
 })
