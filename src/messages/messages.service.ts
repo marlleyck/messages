268206f311bbaf0a3de75585f243c01a59a80872
marlleyck <marlleyck@hotmail.com>
2023-01-28 01:39:11 -0300

    feat: add not found error to findById

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -18,8 +18,12 @@ export class MessagesService {
     return this.messages;
   }
 
-  findById(id: number) {
-    return this.messages.find((message) => message.id === id);
+  async findById(id: number) {
+    const message = this.messages.find((msg) => msg.id === id);
+
+    if (!message) {
+      throw Error(`Message with ID '${id}' not found!`);
+    }
   }
 
   create(message: MessageType) {
